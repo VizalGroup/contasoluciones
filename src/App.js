69d807d98cd4d48651f089d33a98cf3d7d91a7c1
Alejandro Villamayor <avillamayor720@gmail.com>
2023-10-09 19:36:17 -0300

    Inicio de proyecto

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Login from './Components/Login/Login'
+import Home from "./Components/Home/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const PrivateRoute = ({ element }) => {
+  
+  // Función para verificar si el usuario ha iniciado sesión o no (puedes implementar tu lógica aquí).
+  const checkLoggedIn = () => {
+    // Para este ejemplo, asumiremos que el usuario ha iniciado sesión si hay un nombre de usuario en el almacenamiento local.
+    return !!localStorage.getItem('username');
+  };
+
+  // Comprueba si el usuario ha iniciado sesión o no.
+  const loggedIn = checkLoggedIn();
+
+  return loggedIn ? element : <Navigate to="/" />;
+};
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import loginConfig from "./credentials";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+  const expectedUsername = loginConfig.REACT_APP_USERNAME;
+  const expectedPassword = loginConfig.REACT_APP_PASSWORD;
+
+  const handleLogin = () => {
+    if (username === expectedUsername && password === expectedPassword) {
+      setLoginError(false);
+      localStorage.setItem("username", username);
+      navigate("/home");
+    } else {
+      setLoginError(true);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh", // Asegura que el contenido esté centrado verticalmente
+      }}
+    >
+      <img
+        src="null"
+        className="img-fluid"
+        alt="logo"
+        style={{ width: "30vh" }}
+      />
+      <div style={{ width: "30vh", marginTop: "20px" }}>
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Ingrese su usuario"
+          aria-label="default input example"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <label for="inputPassword5" class="form-label"></label>
+        <input
+          type="password"
+          id="inputPassword5"
+          placeholder="Ingrese su contraseña"
+          class="form-control"
+          aria-describedby="passwordHelpBlock"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {loginError && (
+          <div className="form-text" style={{ color: "red" }}>
+            Credenciales incorrectas
+          </div>
+        )}
+        <div id="passwordHelpBlock" class="form-text">
+          En caso de no poder iniciar sesión correctamente comunicarse a
+          vizalgroup0@gmail.com.
+        </div>
+      </div>
+
+      <button
+        style={{ marginTop: "20px" }}
+        type="button"
+        className="btn btn-primary"
+        onClick={handleLogin}
+      >
+        Iniciar sesión
+      </button>
+    </div>
+  );
+}
+
+export default Login;
