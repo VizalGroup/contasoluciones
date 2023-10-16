7b443b6fbeb56a1cffa5849fe8e0fed14b16f57f
Alejandro Villamayor <avillamayor720@gmail.com>
2023-10-16 12:50:37 -0300

    Agregar cleinte, navegacion entre secciones

--- src/Components/Clientes/ClienteForm/ClienteForm.jsx
+++ src/Components/Clientes/ClienteForm/ClienteForm.jsx
@@ -1,123 +1,179 @@
-import React, { useState } from 'react'
-import {PostCliente} from "../../../Redux/actions";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { PostCliente } from "../../../Redux/actions";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { Link } from "react-router-dom";
+import styles from "./ClienteForm.module.css";
+import DatePicker  from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.module.css'
+
 
 export default function ClienteForm() {
-    const dispatch = useDispatch();
-    const [formData, setFormData] = useState({
-        nombre: '',
-        cuit: '',
-        cai: '',
-        inicio_actividades: '',
-        direccion: '',
-        numero_ingresos_brutos: '',
-        numero_controladora_fiscal: '',
-      });
+  const dispatch = useDispatch();
 
-      const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-          ...formData,
-          [name]: value,
-        });
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        // Envía los datos del cliente al servidor utilizando la acción PostCliente
-        dispatch(PostCliente(formData));
-        
-        // Puedes reiniciar el estado del formulario si lo deseas
-        setFormData({
-          nombre: '',
-          cuit: '',
-          cai: '',
-          inicio_actividades: '',
-          direccion: '',
-          numero_ingresos_brutos: '',
-          numero_controladora_fiscal: '',
-        });
-      };
+  // Estado local para los campos del formulario
+  const [formData, setFormData] = useState({
+    nombre: "",
+    cuit: "",
+    cai: "",
+    inicio_actividades: "",
+    direccion: "",
+    numero_ingresos_brutos: "",
+    numero_controladora_fiscal: "",
+  });
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-
-      return (
-        <div>
-          <h2>Agregar Cliente</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="nombre">Nombre:</label>
-              <input
-                type="text"
-                id="nombre"
-                name="nombre"
-                value={formData.nombre}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="cuit">CUIT:</label>
-              <input
-                type="text"
-                id="cuit"
-                name="cuit"
-                value={formData.cuit}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="cai">CAI:</label>
-              <input
-                type="text"
-                id="cai"
-                name="cai"
-                value={formData.cai}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="inicio_actividades">Inicio de Actividades:</label>
-              <input
-                type="text"
-                id="inicio_actividades"
-                name="inicio_actividades"
-                value={formData.inicio_actividades}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="direccion">Dirección:</label>
-              <input
-                type="text"
-                id="direccion"
-                name="direccion"
-                value={formData.direccion}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="numero_ingresos_brutos">Nro Ingresos Brutos:</label>
-              <input
-                type="text"
-                id="numero_ingresos_brutos"
-                name="numero_ingresos_brutos"
-                value={formData.numero_ingresos_brutos}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="numero_controladora_fiscal">Nro Controladora Fiscal:</label>
-              <input
-                type="text"
-                id="numero_controladora_fiscal"
-                name="numero_controladora_fiscal"
-                value={formData.numero_controladora_fiscal}
-                onChange={handleInputChange}
-              />
-            </div>
-            <button type="submit">Agregar</button>
-          </form>
-        </div>
-      );
+  const handleCUITKeyPress = (e) => {
+    const pattern = /[0-9-]/;
+    const inputChar = String.fromCharCode(e.charCode);
+    if (!pattern.test(inputChar)) {
+      e.preventDefault();
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Envía los datos del cliente al servidor utilizando la acción PostCliente
+    dispatch(PostCliente(formData));
+
+    // Puedes reiniciar el estado del formulario si lo deseas
+    setFormData({
+      nombre: "",
+      cuit: "",
+      cai: "",
+      inicio_actividades: "",
+      direccion: "",
+      numero_ingresos_brutos: "",
+      numero_controladora_fiscal: "",
+    });
+  };
+
+  return (
+    <div>
+      <h2 className={styles.title}>Agregar Cliente</h2>
+      <Link to="/clients">
+        <Button
+          component={Link}
+          variant="contained"
+          to="/clients"
+          color="primary"
+          style={{ marginTop: "3vh", marginLeft: "3vw", marginBottom: "3vh" }}
+        >
+          Volver
+        </Button>
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              label="Nombre o razón social"
+              fullWidth
+              name="nombre"
+              value={formData.nombre}
+              onChange={handleInputChange}
+              required
+              inputProps={{
+                maxLength: 200,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="CUIT"
+              fullWidth
+              name="cuit"
+              value={formData.cuit}
+              onChange={handleInputChange}
+              onKeyPress={handleCUITKeyPress}
+              required
+              inputProps={{
+                maxLength: 20,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="CAI"
+              fullWidth
+              name="cai"
+              value={formData.cai}
+              onChange={handleInputChange}
+              onKeyPress={handleCUITKeyPress}
+              required
+              inputProps={{
+                maxLength: 15,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Inicio de Actividades"
+              fullWidth
+              name="inicio_actividades"
+              value={formData.inicio_actividades}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Dirección"
+              fullWidth
+              name="direccion"
+              value={formData.direccion}
+              onChange={handleInputChange}
+              required
+              inputProps={{
+                maxLength: 100,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Nro Ingresos Brutos"
+              fullWidth
+              name="numero_ingresos_brutos"
+              value={formData.numero_ingresos_brutos}
+              onChange={handleInputChange}
+              required
+              inputProps={{
+                maxLength: 50,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Nro Controladora Fiscal"
+              fullWidth
+              name="numero_controladora_fiscal"
+              value={formData.numero_controladora_fiscal}
+              onChange={handleInputChange}
+              required
+              inputProps={{
+                maxLength: 50,
+              }}
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          style={{ marginTop: 16 }}
+        >
+          Agregar
+        </Button>
+      </form>
+    </div>
+  );
+}
