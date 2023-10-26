7cd85a041d5ea7dda457b94c5a48692ea138482a
Alejandro Villamayor <avillamayor720@gmail.com>
2023-10-26 15:26:37 -0300

    Modelos de Facturas, este  primer push es para que tamara puedaarrancar con sus diferentes diseños

---
+++ src/Components/Facturas/FacturaSimple/FacturaSimple.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router";
+import Styles from "./FacturaSimple.module.css";
+import { GetFacturaDetaill, ClearID } from "../../../Redux/actions";
+
+export default function FacturaSimple() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const facturas = useSelector((state) => state.facturas);
+  const clientes = useSelector((state) => state.clientes);
+  const productos = useSelector((state) => state.productos);
+
+  const { facturaDetail } = useSelector((state) => state);
+  const [clienteFactura, setClienteFactura] = useState();
+  const [productosFactura, setProductosFactura] = useState();
+
+  const EncontrarClienteProductos = () => {
+    let cliente = clientes.find((c) => c.id === facturaDetail.id_cliente);
+    let listaProductos = productos.filter(
+      (p) => p.id_factura === facturaDetail.id
+    );
+    setClienteFactura(cliente);
+    setProductosFactura(listaProductos);
+  };
+
+  // Los console.log están dentro de un bloque condicional
+  if (productosFactura && clienteFactura) {
+    console.log("Productos: ", productosFactura);
+    console.log("Cliente: ", clienteFactura);
+  }
+
+  useEffect(() => {
+    dispatch(ClearID());
+    dispatch(GetFacturaDetaill(id))
+      .then((response) => {
+        EncontrarClienteProductos();
+        console.log("Detalles de la factura:", response.payload);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los detalles de la factura:", error);
+      });
+  }, [dispatch, id]);
+
+  return (
+    <div className={Styles.pageContainer}>
+      {facturaDetail && clienteFactura && productosFactura ? (
+        <div className={Styles.Columna}>
+          <div className={Styles.flexContainer}>
+            <div>
+              <p>{clienteFactura.nombre}</p>
+              <p>{clienteFactura.direccion}</p>
+              <p className={Styles.smallText}>IVA RESPONSABLE INSCRIPTO</p>
+            </div>
+            <p className={Styles.tipoFactura}>A</p>
+            <div>
+              <p>Factura</p>
+              <p className={Styles.smallText}>
+                Nro de Factura: {facturaDetail.nro_factura}
+              </p>
+              <p>Fecha: {facturaDetail.fecha}</p>
+              <p className={Styles.smallText}>
+                C.U.I.T Nº: {clienteFactura.cuit}
+              </p>
+              <p className={Styles.smallText}>
+                ING. BRUTOS: {clienteFactura.numero_ingresos_brutos}
+              </p>
+              <p className={Styles.smallText}>
+                INICIO DE ACTIVIDADES: {clienteFactura.inicio_actividades}
+              </p>
+            </div>
+          </div>
+          <br />
+          <hr />
+          <br />
+          <hr />
+          <p>Productos:</p>
+          {productosFactura.map((producto, index) => (
+          <p key={index}>{producto.concepto}</p>
+          ))}
+        </div>
+      ) : (
+        <h4>Loading...</h4>
+      )}
+    </div>
+  );
+}
+{
+}
--- src/Components/Facturas/FacturasTable/FacturasTable.jsx
+++ src/Components/Facturas/FacturasTable/FacturasTable.jsx
@@ -1,48 +1,43 @@
-
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // Material y estilos
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
-import { Grid } from '@mui/material';
-import Button from 'react-bootstrap/Button';
-import Styles from './FacturasTable.module.css';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import { Grid } from "@mui/material";
+import Button from "react-bootstrap/Button";
+import Styles from "./FacturasTable.module.css";
+import { Link } from 'react-router-dom';
 
 // Date Pikers
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import dayjs from 'dayjs';
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
 
 // Acciones
-import { GetClientes, GetFacturas, UpdateFactura, DeleteFactura, GetProductos } from '../../../Redux/actions';
+import {
+  GetClientes,
+  GetFacturas,
+  UpdateFactura,
+  DeleteFactura,
+  GetProductos,
+} from "../../../Redux/actions";
 
 const FacturasTable = () => {
   const facturas = useSelector((state) => state.facturas);
   const clientes = useSelector((state) => state.clientes);
   const productos = useSelector((state) => state.productos);
-  const [fecha, setFecha] = useState('');
+  const [fecha, setFecha] = useState("");
   const dispatch = useDispatch();
-
-  const [searchTerm, setSearchTerm] = useState('');
-
-  // Filtrar las facturas basadas en el término de búsqueda
-  const filteredFacturas = facturas.filter((factura) => { // Filtra por todas las propiedades excepto 'fecha'
-    return Object.values(factura).some(
-      (value) =>
-        (typeof value === 'string' &&
-        value.toLowerCase().includes(searchTerm.toLowerCase()) )
-        || typeof value === 'number'
-    );
-  });
+  const [selectedOption, setSelectedOption] = useState("facturasimple");
 
   const handleDelete = (facturaId) => {
     dispatch(DeleteFactura(facturaId));
@@ -56,115 +51,135 @@ const FacturasTable = () => {
   };
 
   const CalcularSubtotal = (id_factura) => {
-    if(productos.length > 0){
-      let productosFactura = productos.filter(p => p.id_factura === id_factura);
+    if (productos.length > 0) {
+      let productosFactura = productos.filter(
+        (p) => p.id_factura === id_factura
+      );
       let sumatoria = 0;
-      for(let i=0; i < productosFactura.length; i++){
+      for (let i = 0; i < productosFactura.length; i++) {
         sumatoria += Number(productosFactura[i].subtotal);
       }
       return sumatoria;
     } else {
-      return
+      return;
     }
   };
 
   const CalcularImporte = (id_factura) => {
-    if(productos.length > 0){
-      let productosFactura = productos.filter(p => p.id_factura === id_factura);
+    if (productos.length > 0) {
+      let productosFactura = productos.filter(
+        (p) => p.id_factura === id_factura
+      );
       let sumatoria = 0;
-      for(let i=0; i < productosFactura.length; i++){
+      for (let i = 0; i < productosFactura.length; i++) {
         sumatoria += Number(productosFactura[i].importe);
       }
       return sumatoria;
     } else {
-      return
+      return;
     }
   };
 
   const ClienteFactura = (id_cliente) => {
-    if(clientes.length > 0){
-      let cliente = clientes.find(c => c.id === id_cliente);
+    if (clientes.length > 0) {
+      let cliente = clientes.find((c) => c.id === id_cliente);
       let nombreCliente = cliente.nombre;
-      return nombreCliente
+      return nombreCliente;
     } else {
-      return
+      return;
     }
   };
 
   // SETEO DE LA FECHA para buscar
   const handleFechaChange = (date) => {
     if (date) {
-      const fechaSeleccionada = dayjs(date).format('DD-MM-YYYY'); // Asegúrate de que la fecha tenga el formato correcto
-      setFecha(fechaSeleccionada)
+      const fechaSeleccionada = dayjs(date).format("DD-MM-YYYY"); // Asegúrate de que la fecha tenga el formato correcto
+      setFecha(fechaSeleccionada);
     }
-    return
-};
+    return;
+  };
 
-  useEffect(()=>{
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  useEffect(() => {
     dispatch(GetFacturas());
     dispatch(GetClientes());
     dispatch(GetProductos());
-  },[dispatch]);
+  }, [dispatch]);
 
   return (
     <div>
       {/* FECHA */}
       <Grid item xs={12} sm={6}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DatePicker']}>
-              <DatePicker 
-                label="Fecha"
-                variant="outlined"
-                value={fecha}
-                onChange={(date) => handleFechaChange(date)}
-                format="DD-MM-YYYY"
-              />
-            </DemoContainer>
+          <DemoContainer components={["DatePicker"]}>
+            <DatePicker
+              label="Fecha"
+              variant="outlined"
+              value={fecha}
+              onChange={(date) => handleFechaChange(date)}
+              format="DD-MM-YYYY"
+            />
+          </DemoContainer>
         </LocalizationProvider>
       </Grid>
 
+      <select value={selectedOption} onChange={handleSelectChange}>
+        <option value="facturasimple">Simple</option>
+        <option value="facturalogo">Con Logo</option>
+        <option value="facturaqr">Con QR</option>
+        <option value="facturalogoyqr">Logo y QR</option>
+        <option value="facturamoderna">Moderna</option>
+      </select>
+
+
       <div>
-      <TableContainer component={Paper} className={Styles.customTable}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Fecha</TableCell>
-              <TableCell>Nro Factura</TableCell>
-              <TableCell>Cliente</TableCell>
-              <TableCell>Subtotal</TableCell>
-              <TableCell>Importe</TableCell>
-              <TableCell>Acciones</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {facturas.map((factura) => (
-              <TableRow key={factura.id}>
-                <TableCell>{factura.id}</TableCell>
-                <TableCell>{factura.fecha}</TableCell>
-                <TableCell>{factura.nro_factura}</TableCell>
-                <TableCell>{ClienteFactura(factura.id_cliente)}</TableCell>
-                <TableCell>{CalcularSubtotal(factura.id)}</TableCell>
-                <TableCell>{CalcularImporte(factura.id)}</TableCell> 
-                <TableCell>
-                  <Button
-                    variant="primary"
-                    onClick={() => handleEdit(factura.id)}
-                  >
-                    Editar
-                  </Button>
-                  <Button
-                    variant="danger"
-                    onClick={() => handleDelete(factura.id)}
-                  >
-                    Borrar
-                  </Button>
-                </TableCell>
+        <TableContainer component={Paper} className={Styles.customTable}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Fecha</TableCell>
+                <TableCell>Nro Factura</TableCell>
+                <TableCell>Cliente</TableCell>
+                <TableCell>Subtotal</TableCell>
+                <TableCell>Importe</TableCell>
+                <TableCell>Acciones</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {facturas.map((factura) => (
+                <TableRow key={factura.id}>
+                  <TableCell>{factura.id}</TableCell>
+                  <TableCell>{factura.fecha}</TableCell>
+                  <TableCell>{factura.nro_factura}</TableCell>
+                  <TableCell>{ClienteFactura(factura.id_cliente)}</TableCell>
+                  <TableCell>{CalcularSubtotal(factura.id)}</TableCell>
+                  <TableCell>{CalcularImporte(factura.id)}</TableCell>
+                  <TableCell style={{ textAlign: "center" }}>
+                    <Button
+                      variant="primary"
+                      onClick={() => handleEdit(factura.id)}
+                    >
+                      Editar
+                    </Button>
+                    <Button
+                      variant="danger"
+                      onClick={() => handleDelete(factura.id)}
+                    >
+                      Borrar
+                    </Button>
+                    <Link to={`/${selectedOption}/${factura.id}`}>
+                      <Button variant="dark">Imprimir</Button>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
